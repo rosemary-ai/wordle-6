d16ed41f19ebf3ae9348bb2c4a9a82988f4dccd6
Rosemary Ai <rosemaryai.rtf@gmail.com>
2025-03-17 14:32:27 +1100

    square

--- client/src/@types/index.ts
+++ client/src/@types/index.ts
@@ -8,3 +8,8 @@ export interface FormData {
   username: Account['username']
   password: Account['password']
 }
+
+export interface SquareProps {
+  letter: string,
+  // color:
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,33 @@ import AuthModal from 'components/AuthModal'
 import Header from 'components/Header'
 import logo from 'assets/react.svg'
 import 'styles/ReactWelcome.css'
+import { SquareProps } from '@types'
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      <Grid />
       <ReactWelcome />
       <LoggedInStatus />
       <AuthModal />
+    </div>
+  )
+}
+
+const Square = ({ letter }: SquareProps) => {
+  return (
+    <div className="square">
+      {letter}
+    </div>
+  )
+}
+
+const Grid = () => {
+  return (
+    <div className="grid">
+      {/* TODO: make this a loop like tic tac toe */}
+      <Square letter='a' />
     </div>
   )
 }
@@ -22,6 +41,7 @@ const ReactWelcome = () => {
       <img src={logo} className='ReactWelcome-logo' alt='logo' />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
+        HIII
       </p>
       <a className='ReactWelcome-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
         Learn React
